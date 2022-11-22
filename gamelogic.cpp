4ed2a2e5503588800d72ba47845e545dddf8cff2
RRabayah <Russl2004@gmail.com>
2022-11-22 19:57:12 +0000

    fixed bug where display was not working properly

--- gamelogic.cpp
+++ gamelogic.cpp
@@ -28,43 +28,54 @@ vector<vector<block*>> mapMaker(int width, int height){
     vector<vector<block*>> empty2DVector = vector<vector<block*>>(height, vector<block*>(width, 0));
 
     for(int i=empty2DVector.size()-1;i>=0;--i){
-		for(int j=0;j<empty2DVector[i].size();j++){
-            block *newBlock = new block(j, i);
+		for(unsigned int j=0;j<empty2DVector[i].size();j++){
+            block *newBlock = new block(i, j);
             empty2DVector[i][j] = newBlock;
             //cout << "(" << empty2DVector[i][j]->x <<", " << empty2DVector[i][j]->y << ") ";
         }
         //cout<<endl;
 	}
-    
+
+    //cout << empty2DVector[4][4]->x << endl;
     return empty2DVector;
 };
 
 void displayMap(vector<vector<block*>> map){
     for(int i=map.size()-1;i>=0;--i){
-		for(int j=0;j<map[i].size();j++){
+		for(unsigned int j=0;j<map[i].size();j++){
+
             if(map[i][j]->isWall){
                 cout << "[-]";
-            } 
+            } else {
 
-            if(map[i][j]->isWall == false){
-                cout << "   ";
-            } 
+                if(map[i][j]->isStart){
+                    cout << " S ";
+                } else if(map[i][j]->isEnd){
+                    cout << " X ";
+                } else if(map[i][j]->isWall == 0){
+                    cout << "   ";
+                } 
 
+            }
         }
+
         cout<<endl;
 	}
 }
 
-//Step 1: populate 2-d vector with blocks
-//Step 2: create a display method
-//Step 3: test aforementioned display method
-
+// Environment Logic
+//Step 1: populate 2-d vector with blocks - DONE
+//Step 2: create a display method - DONE
+//Step 3: test aforementioned display method - DONE
+//Step 4: make a start-maker and an end-maker
+//Step 5: implement a randomized A* function for generating random pathways in the obstacle course
+//Step 6: implement feathering
+//Step 7: Maybe add controls for manual agent control?
+// Environment logic checklist ends here
 
 int main(){
 
-    vector<vector<block*>> sample = mapMaker(7, 26);
-    sample[2][7]->isWall = false;
-    sample[5][15]->isWall = false;
+    vector<vector<block*>> sample = mapMaker(5, 5);
     displayMap(sample);
 
     return 0;
